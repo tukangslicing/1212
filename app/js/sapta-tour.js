3c6e9f7cce8e1df1ac376b1ffb46323f8bc4cc1c
Hengkiardo <hengki.sihombing@gmail.com>
2015-04-11 21:53:29 +0700

    tours page and all assets

--- app/js/sapta-tour.js
+++ app/js/sapta-tour.js
@@ -11,4 +11,16 @@ $(function () {
     slideshow: false,
     directionNav: false
   });
+
+  $('.tour-list .tour-item').hover(function (e) {
+    // console.log(this);
+    // var $curent = $(this);
+    // var $rolloverBlock = $curent.find('.rollover');
+    // $rolloverBlock.animate({
+    //   top: '+=50'
+    // }, 5000, function () {
+    //   console.log('complete');
+    // });
+
+  })
 })
