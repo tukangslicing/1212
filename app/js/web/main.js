c4f593cbff2144fce0c95155e90138df031e9a41
Hengkiardo <hengki.sihombing@gmail.com>
2015-04-14 00:24:17 +0700

    tour and flight form

--- app/js/web/main.js
+++ app/js/web/main.js
@@ -12,18 +12,49 @@ $(function () {
     directionNav: false
   });
 
-  $(window).on('scroll', function () {
-    if ($('.nav-go-to').length > 0) {
-      var navHeight = $('.nav-go-to').offset().top;
-      var navSticky = $('.nav-go-to');
+  $('.inline-text form input[type=text]').attr('size', function () {
+    var length = 2;
 
-      if ($(window).scrollTop() > 982) {
-        navSticky.addClass('fixed')
-      } else {
-        navSticky.removeClass('fixed');
-      }
+    if (this.value.length < 1) {
+      $(this).addClass('is-empty');
+    } else {
+      $(this).removeClass('is-empty');
     }
-  });
+
+    if (this.value.length > 5) {
+      length = this.value.length;
+    }
+
+    if (length > 14) {
+      length = length - (length / 6)
+    }
+    return Math.ceil(length);
+
+  }).on('keyup', function () {
+    var length = 2;
+
+    if (this.value.length < 1) {
+      $(this).addClass('is-empty');
+    } else {
+      $(this).removeClass('is-empty');
+    }
+
+    if (this.value.length > 5) {
+      length = this.value.length;
+    }
+
+    if (length > 14) {
+      length = length - (length / 6)
+    }
+
+    if (length === 7) {
+      length = length + 1
+    }
+
+    $(this).attr('size', Math.ceil(length));
+
+
+  }).trigger('keyup');
 
   $('.tour-list .tour-item').hover(function (e) {
     // console.log(this);
@@ -37,3 +68,16 @@ $(function () {
 
   })
 })
+
+$(window).on('scroll', function () {
+  if ($('.nav-go-to').length > 0) {
+    var navHeight = $('.nav-go-to').offset().top;
+    var navSticky = $('.nav-go-to');
+
+    if ($(window).scrollTop() > 982) {
+      navSticky.addClass('fixed')
+    } else {
+      navSticky.removeClass('fixed');
+    }
+  }
+});
