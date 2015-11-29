b708fafd2cf7b20897980b6abcb1ff5e421ac2d3
Hengkiardo <hengki.sihombing@gmail.com>
2015-11-29 22:31:49 +0700

    bomb...

--- app/js/shopee-1212.js
+++ app/js/shopee-1212.js
@@ -1,5 +1,25 @@
 $(window).load(function () {
   'use strict';
+
+  $('#carousel').flexslider({
+    animation: 'slide',
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 244,
+    itemMargin: 0,
+    asNavFor: '#testimony-slider'
+  });
+
+  $('#testimony-slider').flexslider({
+    animation: 'slide',
+    controlNav: false,
+    directionNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: '#carousel'
+  });
+
 });
 
 $(function () {
@@ -11,6 +31,51 @@ $(function () {
     controlNav: false,
     directionNav: true
   });
+
+  var timelineBlocks = $('.cd-timeline-block');
+  var offset = 0.8;
+
+  $(window).on('scroll', function () {
+
+    if (!window.requestAnimationFrame) {
+
+      setTimeout(function () {
+        showBlocks(timelineBlocks, offset);
+      }, 100)
+
+    } else {
+      window.requestAnimationFrame(function () {
+        showBlocks(timelineBlocks, offset);
+      });
+    }
+
+  });
+
+  function hideBlocks(blocks, offset) {
+    blocks.each(function () {
+      if ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) {
+
+        $(this)
+          .find('.cd-timeline-img, .cd-timeline-content')
+          .addClass('is-hidden');
+      }
+    });
+  }
+
+  function showBlocks(blocks, offset) {
+    blocks.each(function () {
+      var checked = $(this).find('.cd-timeline-img').hasClass('is-hidden');
+
+      if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && checked){
+        $(this)
+          .find('.cd-timeline-img, .cd-timeline-content')
+          .removeClass('is-hidden')
+          .addClass('bounce-in');
+      }
+    });
+  }
+
+  hideBlocks(timelineBlocks, offset);
 
 })
 
